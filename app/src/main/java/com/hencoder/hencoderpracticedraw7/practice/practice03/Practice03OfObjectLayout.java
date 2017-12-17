a2322afd7a9eb2753533853b22f5f618edb6092f
humanheima <humanheima@gmail.com>
2017-12-17 11:07:50 +0800

    do practice

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -15,6 +15,7 @@ import com.hencoder.hencoderpracticedraw7.R;
 public class Practice03OfObjectLayout extends RelativeLayout {
     Practice03OfObjectView view;
     Button animateBt;
+    PointF pointF=new PointF();
 
     public Practice03OfObjectLayout(Context context) {
         super(context);
@@ -52,7 +53,9 @@ public class Practice03OfObjectLayout extends RelativeLayout {
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            pointF.x = fraction * (endValue.x - startValue.x);
+            pointF.y = fraction * (endValue.y - startValue.y);
+            return pointF;
         }
     }
 }
